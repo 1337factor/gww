1ff8954fc8000088cb91aab1d2c1cdf9720afd43
Danijel Pancic <danijel.pancic@gmail.com>
2012-05-11 13:31:21 +0200

    added the django project

---
+++ greenww/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'greenww.views.home', name='home'),
+    # url(r'^greenww/', include('greenww.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
